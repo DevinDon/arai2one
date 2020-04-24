6d31d1e9fb8ffb946a6fb152d1c321f8c741c3e0
Devin Don <I.INF@Outlook.com>
2020-04-25 01:16:02 +0800

    feat(page/list): add list page

--- client/src/app/page/list/list.component.ts
+++ client/src/app/page/list/list.component.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  // private client: Aria2;
+
+  items: string[];
+
+  status?: boolean;
+
+  constructor() {
+    console.log('Create list component.');
+    this.client = new Aria2({
+      host: '192.168.0.241',
+      port: 6800,
+      secure: false,
+      secret: 'SECRET',
+      path: '/jsonrpc'
+    });
+  }
 
   ngOnInit(): void {
+    this.client.open()
+      .then(v => this.status = true)
+      .catch(e => this.status = false);
+  }
+
+  getActiveList() {
+    this.client.call('tellActive')
+      .then(v => {
+        this.items = v;
+        console.log(this.items);
+      });
   }
 
 }
