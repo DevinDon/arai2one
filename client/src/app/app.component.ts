8b1e3a8e3fe344add23a343bd4aecdab3a27b2cc
Devin Don <I.INF@Outlook.com>
2020-04-26 06:07:54 +0800

    feat(app): add connect state of status bar

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,7 @@
 // src/app/app.component.ts
 
 import { Component, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Subscription, of } from 'rxjs';
 import { Navigation } from './module/routing/routes';
 import { AppService } from './service/app.service';
 import { Aria2Service } from './service/aria2.service';
@@ -17,6 +17,7 @@ export class AppComponent implements OnDestroy {
 
   title = 'Template';
   navigation: Navigation = { icon: 'ri-home-3-line', link: ['/'], tip: 'Home' };
+  state?: boolean;
 
   private subscriptions: Subscription[] = [];
 
@@ -32,7 +33,11 @@ export class AppComponent implements OnDestroy {
         })
     );
     this.aria2.connect()
-      .subscribe();
+      .pipe(catchError(e => {
+        this.state = false;
+        return of(false);
+      }))
+      .subscribe(v => this.state = !!v);
   }
 
   ngOnDestroy(): void {
