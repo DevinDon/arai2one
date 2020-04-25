3ca8065af0c3f480525bc0f06afc31aa7afab020
Devin Don <I.INF@Outlook.com>
2020-04-26 02:56:40 +0800

    feat(service/aria2): subscriptions works

--- client/src/app/page/list/list.component.ts
+++ client/src/app/page/list/list.component.ts
@@ -9,53 +9,58 @@ import { Aria2Service } from 'src/app/service/aria2.service';
 })
 export class ListComponent implements OnInit {
 
-  private client: Aria2;
+  test: number = 0;
 
   tasks: Task[];
   activeTasks: Task[];
   waitingTasks: Task[];
-  stoppendTasks: Task[];
+  stoppedTasks: Task[];
 
   status?: boolean;
 
   constructor(
     private aria2: Aria2Service
-  ) {
-    console.log('Create list component.');
-    this.client = aria2.client;
-  }
+  ) { }
 
-  ngOnInit(): void {
-    this.aria2.connect()
-      .then(v => this.status = true)
+  async ngOnInit() {
+    await this.aria2.connect()
+      .then(v => {
+        this.status = true;
+        this.aria2.syncActive()
+          .subscribe(tasks => this.activeTasks = tasks);
+        this.aria2.syncStopped()
+          .subscribe(tasks => this.stoppedTasks = tasks);
+        this.aria2.syncWaiting()
+          .subscribe(tasks => this.waitingTasks = tasks);
+      })
       .catch(e => {
         console.error(e);
         this.status = false;
       });
   }
 
-  async getActiveTasks() {
-    this.activeTasks = await this.client.tellActive();
-    console.log(this.activeTasks);
-    return this.activeTasks;
-  }
+  // async getActiveTasks() {
+  //   this.activeTasks = await this.client.tellActive();
+  //   console.log(this.activeTasks);
+  //   return this.activeTasks;
+  // }
 
-  async getWaitingTasks(offset = 0, total = 10) {
-    this.waitingTasks = await this.client.tellWaiting(offset, total);
-    console.log(this.waitingTasks);
-    return this.waitingTasks;
-  }
+  // async getWaitingTasks(offset = 0, total = 10) {
+  //   this.waitingTasks = await this.client.tellWaiting(offset, total);
+  //   console.log(this.waitingTasks);
+  //   return this.waitingTasks;
+  // }
 
-  async getAllTasks(offset = 0, total = 10) {
-    this.activeTasks = await this.client.tellActive();
-    this.waitingTasks = await this.client.tellWaiting(offset, total);
-    this.stoppendTasks = await this.client.tellStopped(offset, total);
-    this.tasks = ([] as Task[]).concat(this.activeTasks)
-      .concat(this.waitingTasks)
-      .concat(this.stoppendTasks)
-      .filter(task => task.files[0]?.path);
-    console.log(this.tasks);
-    return this.tasks;
-  }
+  // async getAllTasks(offset = 0, total = 10) {
+  //   this.activeTasks = await this.client.tellActive();
+  //   this.waitingTasks = await this.client.tellWaiting(offset, total);
+  //   this.stoppendTasks = await this.client.tellStopped(offset, total);
+  //   this.tasks = ([] as Task[]).concat(this.activeTasks)
+  //     .concat(this.waitingTasks)
+  //     .concat(this.stoppendTasks)
+  //     .filter(task => task.files[0]?.path);
+  //   console.log(this.tasks);
+  //   return this.tasks;
+  // }
 
 }
--- client/src/app/service/aria2.service.ts
+++ client/src/app/service/aria2.service.ts
@@ -1,25 +1,54 @@
 import { Injectable } from '@angular/core';
-import { Aria2 } from '@iinfinity/aria2';
+import { Aria2, Option } from '@iinfinity/aria2';
+import { fromPromise } from 'rxjs/observable/fromPromise';
+import { interval } from 'rxjs/observable/interval';
+import { map, share, switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Aria2Service {
 
-  public readonly client: Aria2;
+  public client: Aria2;
 
-  constructor() {
-    this.client = new Aria2({
-      host: '192.168.0.241',
-      port: 6800,
-      path: '/jsonrpc',
-      secure: false,
-      secret: 'SECRET'
-    });
+  constructor() { }
+
+  async connect(option: Option = {
+    host: '192.168.0.241',
+    port: 6800,
+    path: '/jsonrpc',
+    secure: false,
+    secret: 'SECRET'
+  }) {
+    this.client = new Aria2(option);
+    return this.client.connect();
   }
 
-  connect() {
-    return this.client.connect();
+  syncActive() {
+    return interval(1500)
+      .pipe(
+        switchMap(_ => fromPromise(this.client.tellActive())),
+        map(tasks => tasks.filter(task => task.files[0]?.path)),
+        share()
+      );
+  }
+
+  syncWaiting(offset = 0, total = 100) {
+    return interval(1500)
+      .pipe(
+        switchMap(_ => fromPromise(this.client.tellWaiting(offset, total))),
+        map(tasks => tasks.filter(task => task.files[0]?.path)),
+        share()
+      );
+  }
+
+  syncStopped(offset = 0, total = 100) {
+    return interval(1500)
+      .pipe(
+        switchMap(_ => fromPromise(this.client.tellStopped(offset, total))),
+        map(tasks => tasks.filter(task => task.files[0]?.path)),
+        share()
+      );
   }
 
 }
