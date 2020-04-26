185b889bf5d5e8f8125d15e0e41ddd5cc25ac280
Devin Don <I.INF@Outlook.com>
2020-04-26 17:17:15 +0800

    feat(dialog/add-task): 更新视图

--- client/src/app/component/add-task-dialog/add-task-dialog.component.ts
+++ client/src/app/component/add-task-dialog/add-task-dialog.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-task-dialog',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddTaskDialogComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
 
-  ngOnInit(): void {
+  constructor() {
+    this.form = new FormGroup({
+      name: new FormControl(''),
+      uri: new FormControl('', [Validators.required, Validators.maxLength(5000)])
+    });
   }
+
+  ngOnInit() { }
 
 }
