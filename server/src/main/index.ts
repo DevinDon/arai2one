1ce0a8cc7032b7d3676e6d433e4ee8a08f04f933
Devin Don <I.INF@Outlook.com>
2020-04-28 23:10:19 +0800

    feat(controller/search): add search method

--- server/src/main/index.ts
+++ server/src/main/index.ts
@@ -1,6 +1,7 @@
 import { CORSHandler, Rester } from '@rester/core';
 import { MovieEntity } from './movie/movie.model';
 import { MovieView } from './movie/movie.view';
+import { SearchView } from './search/search.view';
 import { SummaryEntity } from './summary/summary.model';
 
 const rester = new Rester()
@@ -8,7 +9,7 @@ const rester = new Rester()
   .setEntities([MovieEntity, SummaryEntity])
   .end()
   .configViews
-  .add(MovieView)
+  .add(MovieView, SearchView)
   .end()
   .configHandlers
   .add(CORSHandler)
--- server/src/main/search/search.controller.ts
+++ server/src/main/search/search.controller.ts
@@ -1,16 +1,61 @@
-import { Controller } from '@rester/core';
+import { DoubanCrawler, Summary } from '@iinfinity/movie-crawler';
+import { Controller, Inject } from '@rester/core';
+import { MovieController } from '../movie/movie.controller';
 import { SummaryEntity } from '../summary/summary.model';
 
 @Controller()
 export class SearchController {
 
-  async search(keyword: string): Promise<SummaryEntity[]> {
-    return SummaryEntity
+  @Inject() private douban!: DoubanCrawler;
+  @Inject() private movieController!: MovieController;
+
+  async search(keyword: string): Promise<Summary[]> {
+    const summaryFromDB = await SummaryEntity
       .find({
         where: {
           title: { $regex: new RegExp(`.*${keyword}.*`) }
         }
       });
+    // 数据库中有记录则返回
+    if (summaryFromDB && summaryFromDB.length) {
+      console.log('Summary in DB: ', summaryFromDB);
+      return summaryFromDB;
+    }
+    console.log('Not in DB');
+    // 没有，尝试爬取豆瓣的搜索信息
+    const summaryFromDouban = await this.douban.search(keyword);
+    // 整理数据，异步后台
+    summaryFromDouban.map<Promise<Summary | undefined>>(async v => {
+      // 尝试从数据库中获取电影信息，或者爬取
+      const movieFromDB = await this.movieController.getDetail(v.id);
+      // 存在电影信息，合并
+      const summary = movieFromDB && {
+        id: v.id,
+        title: v.title,
+        image: v.img,
+        type: v.type,
+        year: +v.year,
+        rating: movieFromDB.rating.star,
+        hot: movieFromDB.rating.total,
+        keywords: movieFromDB.types,
+        description: movieFromDB.description
+      } || undefined;
+      // 写入数据库 Summary
+      if (summary) { await SummaryEntity.insert(summary); }
+      return summary;
+    });
+    // 只返回从豆瓣爬取的信息
+    return summaryFromDouban.map<Summary>(v => ({
+      id: v.id,
+      title: v.title,
+      image: v.img,
+      type: v.type,
+      year: +v.year,
+      rating: 0,
+      hot: 0,
+      keywords: [],
+      description: ''
+    }));
   }
 
 }
