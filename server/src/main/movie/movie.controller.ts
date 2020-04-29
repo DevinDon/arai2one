d1c6e07a998a98db6e83c1e13e39ca7e40a1eeb6
Devin Don <I.INF@Outlook.com>
2020-04-29 17:44:24 +0800

    perf(server/admin): optimize logger

--- server/src/main/movie/movie.controller.ts
+++ server/src/main/movie/movie.controller.ts
@@ -13,13 +13,15 @@ export class MovieController {
     const detailInDB = await MovieEntity.findOne({ id });
     // 首先尝试从数据库获取
     if (detailInDB) {
-      logger.info(`${id}: Hit cache from movie`);
+      logger.info(`Movie ${id}: Hit cache from movie`);
       return detailInDB;
     }
     // 如果数据库不存在，尝试爬取
-    logger.info(`${id}: Crawler working for movie`);
-    const detailFromDouban = await this.douban.movie(id);
-    const detailFromPianku = await this.pianku.movieByID(id);
+    logger.info(`Movie ${id}: Crawler working for movie`);
+    const detailFromDouban = await this.douban.movie(id)
+      .catch(e => (logger.info(`Movie ${id}: Fetch failed from Douban`), undefined));
+    const detailFromPianku = await this.pianku.movieByID(id)
+      .catch(e => (logger.info(`Movie ${id}: Fetch failed from Pianku`), undefined));
     // 如果爬到了 片库 的下载资源，合并
     if (detailFromPianku) {
       const detail = Object.assign<any, Movie | undefined, Pick<Movie, 'downloads' | 'links'>>(
@@ -30,10 +32,10 @@ export class MovieController {
       MovieEntity.update({ id }, detail)
         .then(v => {
           if (v.affected) {
-            logger.info(`${id}: Update movie with downloads`);
+            logger.info(`Movie ${id}: Update movie with downloads`);
           } else {
             MovieEntity.insert(detail)
-              .then(v => logger.info(`${id}: Insert movie with downloads`));
+              .then(v => logger.info(`Movie ${id}: Insert movie with downloads`));
           }
         });
       return detail;
@@ -43,10 +45,10 @@ export class MovieController {
       MovieEntity.update({ id }, detailFromDouban)
         .then(v => {
           if (v.affected) {
-            logger.info(`${id}: Update movie without downloads`);
+            logger.info(`Movie ${id}: Update movie without downloads`);
           } else {
             MovieEntity.insert(detailFromDouban)
-              .then(v => logger.info(`${id}: Insert movie without downloads`));
+              .then(v => logger.info(`Movie ${id}: Insert movie without downloads`));
           }
         });
     }
